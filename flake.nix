d5d0f881ebe8028db8efb72460256e2d8c5dab6c
Rodrigo Belem <rodrigo.belem@gmail.com>
2023-01-14 19:41:07 -0400

    Keep only my devices architectures

--- flake.nix
+++ flake.nix
@@ -20,13 +20,8 @@
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       inherit (self) outputs;
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "aarch64-linux"
-        "i686-linux"
-        "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     rec {
       # Your custom packages
