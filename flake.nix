27d3d8340d14fc3eb8c0c56f151a09585be0faa4
Rodrigo Belem <rodrigo.belem@gmail.com>
2023-01-14 19:52:29 -0400

    Add the nixos hardware config modules

--- flake.nix
+++ flake.nix
@@ -9,8 +9,7 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # TODO: Add any other flake you might need
-    # hardware.url = "github:nixos/nixos-hardware";
+    hardware.url = "github:nixos/nixos-hardware";
 
     # Shameless plug: looking for a way to nixify your themes and make
     # everything match nicely? Try nix-colors!
