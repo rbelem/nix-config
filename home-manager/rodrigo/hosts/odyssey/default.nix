744b8dd4939f15d7af129801262f1b7390c3c69f
Rodrigo Belem <rodrigo.belem@gmail.com>
2023-01-16 18:23:33 -0400

    Set my username on home-manager config

--- home-manager/rodrigo/hosts/odyssey/default.nix
+++ home-manager/rodrigo/hosts/odyssey/default.nix
@@ -40,10 +40,9 @@
     };
   };
 
-  # TODO: Set your username
   home = {
-    username = "your-username";
-    homeDirectory = "/home/your-username";
+    username = "rodrigo";
+    homeDirectory = "/home/rodrigo";
   };
 
   # Add stuff for your user as you see fit:
