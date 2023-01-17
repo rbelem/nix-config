55e28b465f1ba6a5afe545cff13275bb8ba28a63
Rodrigo Belem <rodrigo.belem@gmail.com>
2023-01-17 18:35:05 -0400

    Until I fix it, disabling nvidia prime

--- nixos/hosts/odyssey/default.nix
+++ nixos/hosts/odyssey/default.nix
@@ -3,7 +3,7 @@
 
   imports = [
     inputs.hardware.nixosModules.common-cpu-intel
-    inputs.hardware.nixosModules.common-gpu-nvidia
+    inputs.hardware.nixosModules.common-gpu-nvidia-nonprime
     inputs.hardware.nixosModules.common-pc-ssd
 
 
