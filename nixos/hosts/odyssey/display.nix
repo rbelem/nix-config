bbbc9a031b79c035c8c9615fc3350467049ee8da
Rodrigo Belem <rodrigo.belem@gmail.com>
2023-01-29 16:39:39 -0400

    Make wayland work with nvidia

--- nixos/hosts/odyssey/display.nix
+++ nixos/hosts/odyssey/display.nix
@@ -1,4 +1,7 @@
 { config, ... }: {
+  # Make wayland work with nvidia
+  boot.kernelParams = [ "nvidia-drm.modeset=1" ];
+
   services.xserver.enable = true;
 
   services.xserver.videoDrivers = [ "nvidia" ];
