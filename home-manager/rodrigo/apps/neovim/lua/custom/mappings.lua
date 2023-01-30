f706875bfeea5e8e0c59d3ec7e62f2fd7cde2471
Rodrigo Belem <rodrigo.belem@gmail.com>
2023-01-30 13:55:49 -0400

    Disable a few nvchad default mappings

--- home-manager/rodrigo/apps/neovim/lua/custom/mappings.lua
+++ home-manager/rodrigo/apps/neovim/lua/custom/mappings.lua
@@ -3,9 +3,30 @@ local M = {}
 
 -- add this table only when you want to disable default keys
 M.disabled = {
+  i = {
+    -- go to  beginning and end
+    ["<C-b>"] = "",
+    ["<C-e>"] = "",
+
+    -- navigate within insert mode
+    ["<C-h>"] = "",
+    ["<C-l>"] = "",
+    ["<C-j>"] = "",
+    ["<C-k>"] = "",
+  },
+
   n = {
-      ["<leader>h"] = "",
-      ["<C-s>"] = ""
+    -- switch between windows
+    ["<C-h>"] = "",
+    ["<C-l>"] = "",
+    ["<C-j>"] = "",
+    ["<C-k>"] = "",
+
+    -- save
+    ["<C-s>"] = "",
+
+    -- Copy all
+    ["<C-c>"] = "",
   }
 }
 
