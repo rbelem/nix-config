811b7b8456179b08694a4e1ca3fa7cfad112c2c1
Rodrigo Belem <rodrigo.belem@gmail.com>
2023-01-31 21:52:28 -0400

    Tuning nvidia settings for brightness and power management

--- nixos/hosts/odyssey/display.nix
+++ nixos/hosts/odyssey/display.nix
@@ -1,15 +1,18 @@
 { config, ... }: {
+  boot.kernelParams = [ "nvidia.NVreg_EnableBacklightHandler=0" ];
   # Make wayland work with nvidia
   hardware.nvidia = {
     modesetting.enable = true;
+    powerManagement.enable = true;
   };
 
   services.xserver.enable = true;
 
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.enable = true;
+  hardware.opengl.driSupport32Bit = true;
 
   services.xserver.screenSection = ''
-    Option         "RegistryDwords"  "EnableBrightnessControl=1"
+    Option "RegistryDwords" "EnableBrightnessControl=1"
   '';
 }
