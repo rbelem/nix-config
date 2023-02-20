d804a54776bffea84253d23a0b6a9898f9fadfc7
Rodrigo Belem <rodrigo.belem@gmail.com>
2023-02-20 16:05:55 -0400

    Add ipython with vi editing mode

--- home-manager/rodrigo/apps/python/default.nix
+++ home-manager/rodrigo/apps/python/default.nix
@@ -1,6 +1,19 @@
 { pkgs, config, ... }: {
   home.packages = with pkgs; [
+    python310Packages.ipdb
+    python310Packages.ipython
+    python310Packages.ipython_genutils
     python310Packages.pip
     python3Full
   ];
+
+  home.file.".ipython/profile_default/ipython_config.py" = {
+    recursive = true;
+    text = ''
+      c.TerminalInteractiveShell.editor = "vi"
+      c.TerminalInteractiveShell.editing_mode = "vi"
+      c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
+    '';
+  };
+
 }
