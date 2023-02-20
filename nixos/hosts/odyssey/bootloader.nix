4993034853b7ec90aee9df439894b56043b58733
Rodrigo Belem <rodrigo.belem@gmail.com>
2023-02-19 23:35:39 -0400

    Use latest kernel always

--- nixos/hosts/odyssey/bootloader.nix
+++ nixos/hosts/odyssey/bootloader.nix
@@ -1,10 +1,11 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
   boot.loader.timeout = 1;
 
   boot.consoleLogLevel = 0;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelParams = [
     "quiet"
     "splash"
