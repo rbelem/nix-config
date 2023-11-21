d63d0b177549df2871ad21968f279629a06e0977
Rodrigo Belem <rodrigo.belem@gmail.com>
2023-11-21 11:34:03 -0400

    Remove home-manager and sops inputs

--- flake.nix
+++ flake.nix
@@ -10,11 +10,9 @@
     # Shameless plug: looking for a way to nixify your themes and make
     # everything match nicely? Try nix-colors!
     # nix-colors.url = "github:misterio77/nix-colors";
-
-    sops-nix.url = "github:mic92/sops-nix";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... } @ inputs: let
+  outputs = { self, nixpkgs, ... } @ inputs: let
       inherit (self) outputs;
       supportedSystems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
