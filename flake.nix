77adcff348d35eee28302d4eb70b1e19edc41118
Rodrigo Belem <rodrigo.belem@gmail.com>
2023-11-21 19:33:08 -0400

    Switch to the channel unstable small

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
 
     hardware.url = "github:nixos/nixos-hardware";
 
