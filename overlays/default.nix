5525d50432143dd975e867cbcaffa42f0413d164
Rodrigo Belem <rodrigo.belem@gmail.com>
2023-12-05 22:00:20 -0400

    Update devbox to version 0.8.3

--- overlays/default.nix
+++ overlays/default.nix
@@ -8,22 +8,22 @@
   modifications = final: prev: {
     devbox =  prev.devbox.override rec {
       buildGoModule = args: prev.buildGoModule.override { go = prev.go_1_21; } ( args // {
-        version = "0.8.2";
+        version = "0.8.3";
         src = final.fetchFromGitHub {
           owner = "jetpack-io";
           repo = "devbox";
-          rev = "0.8.2";
+          rev = "0.8.3";
           # To update the sha256
           # sha256 = final.lib.fakeHash;
-          sha256 = "sha256-Hh4SfmdR7hujc6Ty+ay8uyl1vkjYuxwa5J5RacqHOAE=";
+          sha256 = "sha256-jxONc6LWoOiRiXQrX6QEJu+ZFY6DVQ9g8A12xfrahO8=";
         };
         # To update the vendorHash
         # vendorHash = final.lib.fakeHash;
-        vendorHash = "sha256-SVChgkPgqhApWDNA1me41zS0hXd1G2oFrL/SsnFiIsg=";
+        vendorHash = "sha256-4HdBTOTQGMAez5gg1R7+M3EqrP0cv8iNjD7ayZD95aU=";
         ldflags = [
           "-s"
           "-w"
-          "-X go.jetpack.io/devbox/internal/build.Version=0.8.2"
+          "-X go.jetpack.io/devbox/internal/build.Version=0.8.3"
         ];
       });
     };
