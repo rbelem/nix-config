f6ed917b75979f786b68f13aae06ee27d59dd9df
Rodrigo Belem <rodrigo.belem@gmail.com>
2024-06-25 23:24:01 -0400

    Update devbox to 0.12.0-devb

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,27 +5,36 @@
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
-  modifications = final: prev: {
+  modifications = final: prev: rec {
+    go = prev.go_1_22.overrideAttrs (finalAttrs: previousAttrs: rec {
+      version = "1.22.4";
+
+      src = final.fetchurl {
+        url = "https://go.dev/dl/go${version}.src.tar.gz";
+        sha256 = "sha256-/tcgZ45yinyjC6jR3tHKr+J9FgKPqwIyuLqOIgCPt4Q=";
+      };
+
+    });
     devbox =  prev.devbox.override rec {
-      buildGoModule = args: prev.buildGoModule.override { go = prev.go_1_22; } ( args // {
-        version = "0.11.0)";
+      buildGoModule = args: prev.buildGoModule.override { go = go; } ( args // {
+        version = "0.12.0-devb)";
         src = final.fetchFromGitHub {
           owner = "jetify-com";
           repo = "devbox";
-          rev = "0.11.0";
+          rev = "0.12.0-devb";
           # To update the sha256
           # sha256 = final.lib.fakeHash;
-          sha256 = "sha256-v2EBN9zp6ssY0hWJQnhsIlRU3L7oOad46bvDUILGIv0=";
+          sha256 = "sha256-+bnFaopmK8Yz2XSkN3wPiipoO5TsRD0IuAKUlx1KvKM=";
 
         };
         # To update the vendorHash
         # vendorHash = final.lib.fakeHash;
-        vendorHash = "sha256-efXYFVs+W6jkShWrU21WCiQqfaNX/9HLD8CxesbkR0s=";
+        vendorHash = "sha256-fuLKo6m/n06W4jyCc4Ki0GLlSIYZNdGFOhpasTd95x0=";
 
         ldflags = [
           "-s"
           "-w"
-          "-X go.jetifiy.com/devbox/internal/build.Version=0.11.0"
+          "-X go.jetify.com/devbox/internal/build.Version=0.12.0-devb"
         ];
       });
     };
